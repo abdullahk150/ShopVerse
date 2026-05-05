16aeda11b3b5b544dbbf54f8bec6c4aa8b2b5946
abdullahk150 <abdullahkaleem150@gmail.com>
2026-05-05 20:53:33 +0500

    Userprofile class updated due to minor errors

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -71,10 +71,7 @@ class VendorRegistrationForm(UserCreationForm):
         return user
 
 
-class UserProfileUpdateForm(forms.ModelForm):
-    first_name = forms.CharField(max_length=50)
-    last_name  = forms.CharField(max_length=50)
-    email      = forms.EmailField()
+
 
     class Meta:
         model  = UserProfile
